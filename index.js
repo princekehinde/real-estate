6ca269b0f51be6acd59d60ca0961c0cbc6883066
prince kehinde <princekehinde2345@gmail.com>
2022-12-15 22:16:05 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const server = require('./src/routes/index');
+const dotenv = require('dotenv').config
+
+
+const port = process.env.PORT || 4000;
+
+
+require('dotenv').config();
+const db = require('./src/config/db');
+
+db()
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((err) => {
+    console.log(`Database connection failed ${err}`);
+  });
+
+server.listen(port, () => {
+  console.log(`Web Service Running on: ${port}`)
+});
